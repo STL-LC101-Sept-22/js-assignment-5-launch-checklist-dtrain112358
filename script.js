095b957f617096cfa332aeedcf93fd6f3ba92d33
unknown <you@example.com>
2022-12-11 20:01:12 -0600

    added more data to function formSubmission

--- script.js
+++ script.js
@@ -14,14 +14,19 @@ window.addEventListener("load", function() {
        console.log(listedPlanets);
        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
    })
- const list = document.getElementById("faultyItems")
- 
- const pilotName = document.getElementId("pilotName")
- const copilotName = document.querySelector("copilotName")
- const fuelLevel = document.querySelector("fuelLevel")
- const cargoMass = document.querySelector("cargoMass")
- 
- const
+ const list = document.getElementById("faultyItems");
+ const launchStatus = document.getElementId("launchStatus");
+ const pilot = document.getElementId("pilotName");
+ const copilot = document.querySelector("copilotName");
+ const fuelLevel = document.querySelector("fuelLevel");
+ const cargoLevel = document.querySelector("cargoMass");
+ const button = document.getElementById("formSubmit");
+
+ button.addEventListener("click", function (event){
+  formSubmission();
+
+ });
+
 
 
 
--- scriptHelper.js
+++ scriptHelper.js
@@ -28,6 +28,34 @@ function validateInput(testInput) {
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    validateInput(pilotName)
+    validateInput(copilotName)
+    validateInput(fuelLevel)
+    validateInput(cargoLevel)
+
+   if (validateInput(pilot) === "Empty" || validateInput(copilot) === "Empty" || validateInput(fuelLevel) === "Empty" || validateInput(cargoLevel) === "Empty"){
+    window.alert("All fields required, Please fill in field.")
+   } else if (validateInput(pilot) === "Not a number" || validateInput(copilot) === "Not a number" || validateInput(fuelLevel) === "Not a number" || validateInput(cargoLevel) === "Not a number") {
+    window.alert("All fields required, Please fill in field.")
+   } else if (validateInput(fuelLevel) === "Is a number" || validateInput(cargoLevel) === "Is a number") {
+    window.alert("All fields required, Please fill in field.")
+   } 
+  
+   list.innerHTML += `
+   <li> 1. The Pilot name is: ${pilot} </li> 
+   <li> 1. The Co-Pilot's name is: ${copilot} </li> 
+   <li> 1. The Fuel Level is:  ${fuelLevel} </li> 
+   <li> 1. The Cargo Level is: ${cargoLevel} </li> 
+   `
+   
+
+
+  
+    TODO // check to make sure all fields are filled out = empty
+    TODO // check validation of form fields = string or number or not a number
+    TODO //update statues of form fields
+    TODO // check if form field values meet required parameters to Launch?
+
    
 }
 
